95d2d2dcc103d090f8b37475d88fb1b82ec1049e
Alexander Pechinkin <pechinkin04@yandex.ru>
2024-05-03 12:47:09 +0400

    Initial Commit

---
+++ MVP-Level One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-Level One
+//
+//  Created by Александр Печинкин on 03.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
